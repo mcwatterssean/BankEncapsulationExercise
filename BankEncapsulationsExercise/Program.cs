8886adecc143205d08c05d0e06996b209202aef1
Sean McWatters <mcwatterssean@gmail.com>
2021-12-09 15:46:41 -0500

    init

---
+++ BankEncapsulationsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulationsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            double amountToDepsoit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDepsoit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance,0:c}");
+
+        }
+    }
+}
